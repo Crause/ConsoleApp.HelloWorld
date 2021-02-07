b40e5f29a8ec1628b1779eab71d38403818692e3
Ktasd Asffa <sapegina212@mail.ru>
2021-02-07 20:07:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp.HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp.HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.ForegroundColor = ConsoleColor.Green;
+
+      Console.WriteLine("Wake up, Neo...");
+      System.Threading.Thread.Sleep(2000);
+
+      Console.Clear();
+      Console.WriteLine("The matrix has you.");
+      System.Threading.Thread.Sleep(2000);
+
+      Console.Clear();
+      Console.WriteLine("Follow the white rabbit.");
+      System.Threading.Thread.Sleep(2000);
+
+      Console.Clear();
+      Console.WriteLine("Knock, knock, Neo.");
+      Console.ReadKey();
+    }
+  }
+}
